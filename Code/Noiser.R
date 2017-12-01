fce8e3d44bff47ae4267d349802528ee2121f755
Marcelin TWORSKI <mtworski@gmail.com>
2017-12-01 13:54:16 +0100

    Code for testing paralllized algorithm

--- Code/Noiser.R
+++ Code/Noiser.R
@@ -5,22 +5,28 @@ args = commandArgs(trailingOnly=TRUE)
 print(args)
 #file = args[1]
 #noisestd = as.integer(args[2])
-file = "../Data/eight.off"
-noisestd = 0.2
+file = "../Data//eight.off"
+noisestd = 0.1
 
 #Reading OFF files
 data <-readLines(file)
-numberOfElements <- strsplit(data[2], " ")
+numberOfElements <- strsplit(data[2], "[ |\t]+")
 Nvertices <- as.integer(numberOfElements[[1]][1])
 Nfaces <- as.integer(numberOfElements[[1]][2])
 
 #Noising data
 for (i in 1:Nvertices){
-  splitedString <- strsplit(data[2 + i], " ")
+  splitedString <- strsplit(data[2 + i], "[ |\t]+")
   splitedString <- as.double(splitedString[[1]])
   splitedString <- splitedString + rnorm(3, 0, noisestd)
   data[2 + i] <- paste(splitedString[1], splitedString[2], splitedString[3])
 }
 
+for (i in 1:Nfaces){
+  splitedString <- strsplit(data[2 + i + Nvertices], "[ |\t]+")
+  splitedString <- as.integer(splitedString[[1]])
+  data[2 + i + Nvertices] <- paste(splitedString[1], splitedString[2], splitedString[3], splitedString[4])
+}
+
 #Saving noised file
-writeLines(data, con = "testnex.off")
+writeLines(data, con = "input.off")
--- Code/filereader.py
+++ Code/filereader.py
@@ -1,26 +1,9 @@
 import numpy as np
-from cvxopt import solvers, matrix
 from scipy import sparse
+from joblib import Parallel, delayed
 import math
+import time
 np.set_printoptions(threshold=np.inf)
-
-
-
-filepath = "testnex.off"
-print("Lecture Fichier")
-(verts, faces) = read_off(open(filepath))
-len_edges = len_edges(verts,faces)
-print("Transformation mesh")
-edges = find_edges(faces, len_edges)
-
-
-longueur = longueur(edges)
-gamma = 1
-beta = 0.001
-lambd = 0.02 * longueur**2 * gamma
-alpha = 0.1 * gamma
-
-
 
 ####################
 #FILE READER
@@ -95,15 +78,15 @@ def param(edges):
         coords = coord(edges[i])
         p12 = coords[1] - coords[0]
         p14 = coords[3] - coords[0]
-        p32 = coords[1] - coords[2]
-        p34 = coords[3] - coords[2]
         p13 = coords[2] - coords[0]
-        l += norm(edges[3]-edges[1])
-        gam += math.acos(np.dot(p13,p12)/(norm(p13)*norm(p12)))
+        l += norm(p13)
+        gam += math.acos(np.dot(p14,p12)/(norm(p14)*norm(p12)))
     l = l/len_edges
+    gam = gam/len_edges
     print("Mean length")
     print("Mean dihedral angle")
     print(l)
+    print(gam)
     return(l,gam)
     
 def find_edges(faces, len_edges):
@@ -145,82 +128,105 @@ def coord(edge):
     return(coords)
     
 def norm(point):
-    return(math.sqrt(np.dot(point,point)))
+    return(math.sqrt(np.dot(point,point.T)))
     
     
 #########################
 #OPERATOR CALCULATION
-    
-def calcops(coords):
-    thetas = np.zeros((len_edges,4), dtype=np.float)
-    deltas = np.zeros((len_edges,2), dtype=np.float)
-    
-    D = np.zeros((len_edges,3), dtype=np.float)
-    Dmatrix = np.zeros((3*len_edges,len_edges * 12), dtype=np.float)
-    R = np.zeros((len_edges,3), dtype=np.float)
-    
-    for i in range(len_edges):
-        coords = coord(edges[i])
-        p12 = coords[1] - coords[0]
-        p14 = coords[3] - coords[0]
-        p32 = coords[1] - coords[2]
-        p34 = coords[3] - coords[2]
-        p13 = coords[2] - coords[0]
-        p31 = -p13
+#timer = 0
+def calcops():
+
+    dcol = []
+    drow = []
+    ddata = []
+    nrow = 3*len_edges
+    ncol = len_edges * 12
+    Parallel(n_jobs = 5) (delayed(subcal)(i) for i in range(len_edges))
         
-        #calcul thetas
-        thetas[i][0] = math.acos(np.dot(p13,p12)/(norm(p13)*norm(p12)))
-        thetas[i][1] = math.acos(np.dot(p14,p12)/(norm(p14)*norm(p12)))
-        thetas[i][2] = math.acos(np.dot(p32,p31)/(norm(p32)*norm(p31)))
-        thetas[i][3] = math.acos(np.dot(p34,p31)/(norm(p34)*norm(p31)))
+    Dmatrix = sparse.coo_matrix((ddata, (drow,dcol)), shape=(nrow, ncol), dtype=np.float)
+    return(Dmatrix)
+
+
+def subcal(i):
+    coords = coord(edges[i])
+        #start = time.time()
+    c1 = coords[0]
+    c2 = coords[1]
+    c3 = coords[2]
+    c4 = coords[2]
+    #timer += time.time() - start
+    p12 = c2 - c1
+    p14 = c4 - c1
+    p32 = c3 - c2
+    p34 = c4 - c3
+    p13 = c3 - c1
+#       p12 = p12.T
+#       p14 = p14.T
+#       p32 = p32.T
+#       p34 = p34.T
+#       p13 = p13.T
+    p31 = -p13
+
+#calcul thetas
+    theta0 = math.acos(np.dot(p13,p12.T)/(norm(p13)*norm(p12)))
+    theta1 = math.acos(np.dot(p14,p12.T)/(norm(p14)*norm(p12)))
+        #thetas[i][2] = math.acos(np.dot(p32,p31)/(norm(p32)*norm(p31)))
+        #thetas[i][3] = math.acos(np.dot(p34,p31)/(norm(p34)*norm(p31)))
         
         #Calcul deltas
-        deltas[i][0] = 0.5 * norm(p12) * norm(p13) * math.sin(thetas[i][0])
-        deltas[i][1] = 0.5 * norm(p14) * norm(p13) * math.sin(thetas[i][1])
+    delta0 = 0.5 * norm(p12) * norm(p13) * math.sin(theta0)
+    delta1 = 0.5 * norm(p14) * norm(p13) * math.sin(theta1)
         
         #Calc D
-        a0 = (deltas[i][0]* np.dot(p34,p13) + deltas[i][1]* np.dot(p31,-p32))/(norm(p13)**2*(deltas[i][0] + deltas[i][1]))
-        a1 = deltas[i][1]/(deltas[i][0] + deltas[i][1])
-        a2 = (deltas[i][0]* np.dot(-p14,p13) + deltas[i][1]* np.dot(p31,p12))/(norm(p13)**2*(deltas[i][0] + deltas[i][1]))
-        a3 = deltas[i][0]/(deltas[i][0] + deltas[i][1])
-        
-        Dmatrix[i][12*i:12*(i+1)] = np.array([a0, 0, 0, a1, 0, 0, a2, 0, 0, a3, 0, 0])
-        Dmatrix[i+1][12*i:12*(i+1)] = np.array([0, a0, 0, 0, a1, 0, 0, a2, 0, 0, a3, 0])
-        Dmatrix[i+2][12*i:12*(i+1)] = np.array([0, 0, a0, 0, 0, a1, 0, 0, a2, 0, 0, a3])
-        D[i] = a0*coords[0] + a1 * coords[1] + a2*coords[2] + a3*coords[3]
-        
-        r_aux = (p12 + p34)
-        R[i] = np.dot(r_aux,r_aux)
-    return(thetas, deltas, D, R, Dmatrix)
+    a0 = (delta0* np.dot(p34,p13.T) + delta1* np.dot(p31,-p32.T))/(norm(p13)**2*(delta0 + delta1))
+    a1 = delta1/(delta0 + delta1)
+    a2 = (delta0 * np.dot(-p14,p13.T) + delta1* np.dot(p31,p12.T))/(norm(p13)**2*(delta0 + delta1))
+    a3 = delta0/(delta0 + delta1)
 
+    drow += 12* [i] + 12* [i+1] + 12* [i+2]
+    dcol +=  3 * list(range(12*i, 12*(i + 1)))
+    ddata += [a0, 0, 0, a1, 0, 0, a2, 0, 0, a3, 0, 0] + [0, a0, 0, 0, a1, 0, 0, a2, 0, 0, a3, 0] + [0, 0, a0, 0, 0, a1, 0, 0, a2, 0, 0, a3]
+    return()
 ###########################
 # PARAMETERS OPTIMIZATION
 def optdelt(D):
-    delt = np.zeros((len_edges,3), dtype=np.float)
-    count = 0
+    delt = np.zeros((3*len_edges,1), dtype=np.float)
+    #count = 0
     for i in range(len_edges):
-        if norm(beta * D[i]) > lambd:
-            delt[i] = D[i]  
-            count += 1 
-    print("Norm L0 of delt:" + str(count))
+        dp = D[3*i:3*(i+1)]
+        if beta * norm(dp.T)  > lambd:
+            delt[3*i:3*(i+1)] = dp
+            #count += 1 
+    #print("Norm L0 of delt:" + str(count))
     return(delt)
 
 def verttoedge (edges):
-    verttoedge = np.zeros((12*len_edges,3*len(verts)), dtype=np.float)
+    nrow= 12*len_edges
+    ncol=3*len(verts)
+    col = []
+    row = []
+    val = []
     for i in range (len_edges):
         for j in range(4):
             pos = edges[i][j]
-            verttoedge[12*i + 3*j][3*pos:3*pos+3] = np.array([1,0,0])
-            verttoedge[12*i + 3*j+1][3*pos:3*pos+3] = np.array([0,1,0])
-            verttoedge[12*i + 3*j+2][3*pos:3*pos+3] = np.array([0,0,1])
+            col += [3*pos, 3*pos +1, 3*pos +2]
+            row += [12*i + 3*j, 12*i + 3*j+1, 12*i + 3*j+2]
+            val += [1,1,1]
+    verttoedge = sparse.coo_matrix((val, (row,col)), shape=(nrow, ncol), dtype=np.uint32)
     return(verttoedge)
     
+
 def Rmatrix ():
-    Rmatrix = np.zeros((3*len_edges, 12*len_edges), dtype=np.float)
+    col = []
+    row = []
+    val = []
+    nrow= 3*len_edges
+    ncol=12*len_edges
     for i in range (len_edges):
-        Rmatrix[3*i][12*i:12*(i+1)]   = np.array([1, 0, 0,-1, 0, 0, 1, 0, 0,-1, 0, 0])
-        Rmatrix[3*i+1][12*i:12*(i+1)] = np.array([0, 1, 0, 0,-1, 0, 0, 1, 0, 0,-1, 0])
-        Rmatrix[3*i+2][12*i:12*(i+1)] = np.array([0, 0, 1, 0, 0,-1, 0, 0, 1, 0, 0,-1])
+        row += 12 * [3*i] +  12 * [3*i+1] + 12 * [3*i+2]
+        col += 3 * list(range(12*i, 12*(i + 1)))
+        val += [1, 0, 0,-1, 0, 0, 1, 0, 0,-1, 0, 0] + [0, 1, 0, 0,-1, 0, 0, 1, 0, 0,-1, 0] + [0, 0, 1, 0, 0,-1, 0, 0, 1, 0, 0,-1]
+    Rmatrix = sparse.coo_matrix((val, (row,col)), shape=(nrow, ncol), dtype=np.float)
     return(Rmatrix)
 
 
@@ -238,82 +244,117 @@ def transformvect(p, verts):
         verts[i][2] =  float(p[3*i + 2])
     return(verts)
     
+filepath = "input.off"
+print("Lecture Fichier")
+(verts, faces) = read_off(open(filepath))
+len_edges = len_edges(verts,faces)
 
+print("Transformation mesh")
+edges = find_edges(faces, len_edges)
+(longueur, gamma) = param(edges)
+beta = 0.001
+lambd = 0.02 * longueur**2 * gamma
+alpha = 0.1 * gamma
 
 print("Calcul Opérateurs")
-(thetas,delta, D, R, Dmatrix) = calcops(edges)
-delt = optdelt(D)
+#Dmatrix = calcops(edges)
 
 print("Calcul Matrices")
 print("Calcul unique R et P")
 r= Rmatrix()
 e= verttoedge(edges)
-r = np.mat(r)
-a = sparse.coo.coo_matrix(np.mat(e))
-Rmat = sparse.coo.coo_matrix(r) * a
-
+Rmat = r * e
 Rmatsym = Rmat.transpose() * Rmat
 Pconstpart = sparse.coo.coo_matrix(np.eye(3*len(verts)))
 pstar = transform1d(verts)
 pstar = np.mat(pstar)
-len(pstar)
 pstar = pstar.T
-len(pstar)
 
 step = 1
+
+###
+#Time mesurement variable 
+t0 = 0
+t1 = 0
+t2 = 0
+t3 = 0
+t4 = 0
+t5 = 0
+t6 = 0
+t01 = 0
+#TODO factoriser sparse.coo.coo_matrix(pstar)
 while (beta < 1000):
 
-    print("Calcul Opérateurs")
-    (thetas,delta, D, R, Dmatrix) = calcops(edges)
-    deltvect = optdelt(D)
-    print("Calcul récurrent D")
-    Dmatrix = sparse.coo.coo_matrix(np.mat(Dmatrix))
-    #Dmatrix = sparse.csc_matrix(Dmatrix)
-    DM = Dmatrix * a
+    #print("Calcul Opérateurs")
+    start =time.time()
+    coords = e * sparse.coo.coo_matrix(pstar)
+    coords = coords.todense()
+    t0 += time.time() - start
+    start =time.time()
+    Dmatrix  = calcops()
+    t01 += time.time() - start
+    #print("Calcul récurrent D")
+    start =time.time()
+    
+    DM = Dmatrix * e
     DMT = DM.transpose()
     DMsym =  DMT * DM
-    #DM = DM.todense()
     
-    print("Calcul récurrent P")
+    t1 += time.time() - start
+                 
+    #print("Optim delta")
+    start = time.time()    
+    DMP = DM * sparse.coo.coo_matrix(pstar)
+    DMP = DMP.todense()
+    delt= optdelt(DMP)
+    t2 += time.time() - start
+    #print("Calcul")
+    start = time.time()
     P = Pconstpart + beta * (DMsym) + alpha * Rmatsym
-    P = P.todense()
-    delt = matrix(transform1d(deltvect))
-    print("Calcul récurrent Q")
-    Qaux = beta *(DMT.todense().dot(delt))
-    Q = - matrix(pstar) - Qaux
-    print("opt")
-    Psolv = matrix(P)
-    Qsolv = matrix(Q)
-    print(len(Qsolv))
-    print(len(Qsolv.T))
-    optres = solvers.qp(Psolv, Qsolv)
-    p = optres['x']
-    p = np.mat(p)
+    t3 += time.time() - start
+    #♠delt = matrix(transform1d(deltvect))
+    start = time.time()
+    Qaux = beta *(DMT * delt)
+    Q =  sparse.coo.coo_matrix(pstar) + Qaux
+    t4 += time.time() - start
+    #print("opt")
+    start = time.time()
+    p = sparse.linalg.spsolve(P, Q)
+    t5 += time.time() - start
+    #optres = solvers.qp(Psolv, Qsolv)
+    #p = optres['x']
+    start = time.time() 
+    p = np.mat(p).T
+    tmp1 = DMP - delt
+    #print("Norme de beta *  D(p) - delt: " + str(beta * tmp1.T*tmp1))
+    tmp2 = pstar - p
+    #print("Norme de p-p*: " + str(tmp2.T * tmp2))
     pstar = p
     verts = transformvect(p, verts)
-    alpha = 0.5*alpha
+    #alpha = 0.5*alpha
     beta = math.sqrt(2) * beta
-    print(beta)
+    
+    #print(beta)
     print(step)
-    write_off(verts, faces, "result"+ str(step)+ ".off")
+    #write_off(verts, faces, "result"+ str(step)+ ".off")
     step +=1
+    t6 += time.time() - start
 
 print("fin")
 print("Creation fichier off result")
 write_off(verts, faces, "result.off")
-write_off(transformvect(verts, pstar), faces, "testytest.off")
+print("Temps")
+print(t0)
+print(t01)
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
+print(t6)
+#
+#len_egdes
 
-
-
-
-
-
-test  = np.matrix([[0,0,1], [0,0,1], [0,1,0]])
-test2 = np.matrix([[0,0,1], [0,0,1], [0,1,0]])
-a = sparse.coo.coo_matrix(test) * sparse.coo.coo_matrix(test2)
-
-print(a.todense())
-print(test* test2)
 
 
 
